06024fa612f6f2f34834c16a2c5e23cef1169876
Otis Simon <osimon@utilitywarehouse.co.uk>
2018-10-14 18:18:58 +0100

    Restart pocket stats

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { CookiesProvider } from 'react-cookie';
-import "react-datepicker/dist/react-datepicker-cssmodules.css";
-
-ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('root'));
-registerServiceWorker();
---
+++ cmd/pocket-stats-server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	cli "github.com/jawher/mow.cli"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	name = "pocket-stats-server"
+	desc = "An API to generate stats based on my pocket account"
+)
+
+func main() {
+	app := cli.App(name, desc)
+
+	app.Action = func() {
+		port := app.Int(cli.IntOpt{
+			Name:   "port",
+			Desc:   "The port to listen on for API GRPC connections",
+			Value:  8080,
+			EnvVar: "PORT",
+		})
+
+		router := mux.NewRouter()
+		router.NewRoute().Path("/").HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("Hit app"))
+			},
+		)
+
+		server := http.Server{
+			Handler: router,
+			Addr:    fmt.Sprintf(":%d", *port),
+		}
+
+		log.Infof("pocker stats started with port %s", server.Addr)
+		if err := server.ListenAndServe(); err != nil {
+			log.WithError(err).Fatal("Fatal error while running HTTP server")
+		}
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.WithError(err).Fatal("fatal error in app")
+	}
+}
